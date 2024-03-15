ccff90dbcea69d0f15eb84878d293bc7452a4655
surya <surya192400@gmail.com>
2024-03-15 23:47:58 +0530

    Header page Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Projects from "./Pages/Projects";
+import Dashboard from "./Pages/Dashboard";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Header from "./Components/Header";
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/project" element={<Projects />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
